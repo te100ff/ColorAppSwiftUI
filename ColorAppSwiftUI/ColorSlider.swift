b1c61b45e1fa8c710fd0e8e7d1e512d7d818b4ae
Stanislav <te100ff@inbox.ru>
2021-06-06 21:35:58 +0300

    Add alert without logic

---
+++ ColorAppSwiftUI/ColorSlider.swift
@@ -0,0 +1,59 @@
+//
+//  Slider.swift
+//  ColorAppSwiftUI
+//
+//  Created by Stanislav Testov on 04.06.2021.
+//
+
+import SwiftUI
+
+struct ColorSlider: View {
+    @Binding var sliderValue: Double
+    @Binding var alertPresented: Bool
+    
+    let color: Color
+    var formatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 0
+        return formatter
+    }
+    
+    var body: some View {
+        HStack {
+            Text(formatter.string(from: NSNumber(value: sliderValue)) ?? "")
+                .bold()
+                .foregroundColor(.white)
+                .frame(width: 50)
+                
+            Slider(value: $sliderValue, in: 0...255, step: 1)
+                .accentColor(color)
+            TextField("", value: $sliderValue, formatter: formatter)
+                .frame(width: 50, height: 30)
+                .background(Color(.white))
+                .multilineTextAlignment(.center)
+                .keyboardType(.decimalPad)
+                .alert(isPresented: $alertPresented)  {
+                    Alert(title: Text("Wrong data"), message: Text("Enter value from 0 to 255"))
+                }
+                
+                
+                
+                
+        }
+    }
+    
+    
+    
+    
+}
+
+struct Slider_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        ZStack {
+            Color(.systemBlue)
+            ColorSlider(sliderValue: .constant(5), color: .green)
+        }
+    }
+}
--- ColorAppSwiftUI/ContentView.swift
+++ ColorAppSwiftUI/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redSliderValue = Double.random(in: 0...255)
+    @State private var greenSliderValue = Double.random(in: 0...255)
+    @State private var blueSliderValue = Double.random(in: 0...255)
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Color(.systemBlue)
+                .ignoresSafeArea()
+            VStack {
+                RoundedRectangle(cornerRadius: 15)
+                    .foregroundColor(Color(
+                        red: redSliderValue/255,
+                        green: greenSliderValue/255,
+                        blue: blueSliderValue/255
+                    )
+                    )
+                    .frame(height: 150)
+                    .overlay(RoundedRectangle(cornerRadius: 15).stroke(Color.white, lineWidth: 5))
+                    .padding()
+                ColorSlider(sliderValue: $redSliderValue, color: .red)
+                ColorSlider(sliderValue: $greenSliderValue, color: .green)
+                ColorSlider(sliderValue: $blueSliderValue, color: .blue)
+                Spacer()
+            }
             .padding()
+        }
     }
 }
 
